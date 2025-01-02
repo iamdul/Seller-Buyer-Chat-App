31c21a4a7f6e6705488cdcd4baa1a60f80b87cbe
iamdul <kalharidulanga@gmail.com>
2025-01-02 07:20:26 +0530

    configure pusher and real-time notifications

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'bio',
+        'picture',
+        'phone_number',
+        'username',
+        'role'
     ];
 
     /**
